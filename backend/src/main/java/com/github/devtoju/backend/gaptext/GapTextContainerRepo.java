1461121f6416057218e8f31e8a04a09e3301d577
devToJu <msg.toju@outlook.de>
2023-04-27 14:48:11 +0200

    Add @Repository to repo

--- backend/src/main/java/com/github/devtoju/backend/gaptext/GapTextContainerRepo.java
+++ backend/src/main/java/com/github/devtoju/backend/gaptext/GapTextContainerRepo.java
@@ -2,6 +2,8 @@ package com.github.devtoju.backend.gaptext;
 
 import com.github.devtoju.backend.gaptext.models.GapTextContainer;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface GapTextContainerRepo extends MongoRepository<GapTextContainer, String> {
 }
