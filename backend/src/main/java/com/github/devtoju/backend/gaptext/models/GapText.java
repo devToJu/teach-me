852dcc26b2b5b16383a0f0867247f2f5f0117588
devToJu <msg.toju@outlook.de>
2023-04-27 12:03:52 +0200

    Add rowPosition to GapText

--- backend/src/main/java/com/github/devtoju/backend/gaptext/models/GapText.java
+++ backend/src/main/java/com/github/devtoju/backend/gaptext/models/GapText.java
@@ -1,10 +1,13 @@
 package com.github.devtoju.backend.gaptext.models;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public record GapText(
         @NotBlank
         String value,
+        @Size(min=1, max=10)
+        int rowPosition,
         boolean isGap
 ) {
 }
--- frontend/src/gaptext/models/GapTextModel.ts
+++ frontend/src/gaptext/models/GapTextModel.ts
@@ -1,4 +1,5 @@
 export type GapTextModel = {
     value: string,
+    rowPosition: number
     isGap: boolean
 }
