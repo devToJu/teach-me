1e8b16eee44a3a3a20cf0908f3e0a7fc3e4ee176
devToJu <msg.toju@outlook.de>
2023-08-20 14:32:48 +0200

    Refactored: Changed response-body if exception occurs.

--- backend/src/main/java/com/github/devtoju/backend/security/jwt/JwtAuthFilter.java
+++ backend/src/main/java/com/github/devtoju/backend/security/jwt/JwtAuthFilter.java
@@ -5,53 +5,100 @@ import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import lombok.RequiredArgsConstructor;
 import org.apache.logging.log4j.util.Strings;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
 
 @Component
 @RequiredArgsConstructor
 public class JwtAuthFilter extends OncePerRequestFilter {
+	private final JwtService jwtService;
 
-    private final JwtService jwtService;
+	private HttpServletResponse response;
+	private HttpServletRequest request;
+	private FilterChain filterChain;
+	private String authHeader;
 
-    @Override
-    protected void doFilterInternal(
-            HttpServletRequest request,
-            HttpServletResponse response,
-            FilterChain filterChain
-    ) throws ServletException, IOException {
-        var authHeader = request.getHeader("Authorization");
-        var isUserIsLoggedIn = SecurityContextHolder.getContext().getAuthentication() != null;
-        var hasNoUserToLogin = Strings.isBlank(authHeader);
+	@Override
+	protected void doFilterInternal(
+			HttpServletRequest request,
+			HttpServletResponse response,
+			FilterChain filterChain
+	) {
+		this.response = response;
+		this.request = request;
+		this.filterChain = filterChain;
+		this.authHeader = request.getHeader("Authorization");
 
-        if (isUserIsLoggedIn || hasNoUserToLogin) {
-            filterChain.doFilter(request, response);
-            return;
-        }
+		if (tryToSkipLogin()) {
+			return;
+		}
 
-        var token = authHeader.replace("Bearer", "").trim();
-        var username = getClaims(token).getSubject();
+		extractUsername().ifPresent(username -> {
+			loginUser(username);
+			doFilter();
+		});
+	}
 
-        loginUser(username);
-        filterChain.doFilter(request, response);
-    }
+	private boolean tryToSkipLogin() {
+		var isUserIsLoggedIn = SecurityContextHolder.getContext().getAuthentication() != null;
+		var hasNoUserToLogin = Strings.isBlank(authHeader);
 
-    private Claims getClaims(String token) {
-        try {
-            return jwtService.validateToken(token);
-        }
-        catch (JwtException | IllegalArgumentException e) {
-            throw new JwtAuthSecurityException(e);
-        }
-    }
+		if (isUserIsLoggedIn || hasNoUserToLogin) {
+			doFilter();
+		}
 
-    private void loginUser(String username) {
-        var authenticationToken = new UsernamePasswordAuthenticationToken(username, "", List.of());
-        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
-    }
+		return isUserIsLoggedIn || hasNoUserToLogin;
+	}
+
+	private Optional<String> extractUsername() {
+		var token = authHeader.replace("Bearer", "").trim();
+
+		try {
+			return Optional.of(getClaims(token).getSubject());
+		} catch (JwtAuthSecurityException e) {
+			createErrorResponse(HttpStatus.UNAUTHORIZED, e.getMessage());
+			return Optional.empty();
+		}
+	}
+
+	private Claims getClaims(String token) {
+		try {
+			return jwtService.validateToken(token);
+		} catch (JwtException | IllegalArgumentException e) {
+			throw new JwtAuthSecurityException(e);
+		}
+	}
+
+	private void loginUser(String username) {
+		var authenticationToken = new UsernamePasswordAuthenticationToken(username, "", List.of());
+		SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+	}
+
+	private void doFilter() {
+		try {
+			filterChain.doFilter(request, response);
+		} catch (FileNotFoundException e) {
+			var error = "Request endpoint " + request.getRequestURI() + " not found!";
+			createErrorResponse(HttpStatus.BAD_REQUEST, error);
+		} catch (IOException | ServletException e) {
+			createErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
+		}
+	}
+
+	private void createErrorResponse(HttpStatus httpStatus, String errorMessage) {
+		try {
+			response.setStatus(httpStatus.value());
+			response.getWriter().write(errorMessage);
+		} catch (IOException e) {
+			// TODO: add to logger
+		}
+	}
 }
